7e5da1700ab0b4dab14e8ae8e2ce2cac66b8f7be
PairZhu <47098840+PairZhu@users.noreply.github.com>
2021-05-04 10:36:49 +0800

    gui框架

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "mbed.h"
-#include "Screen.h"
+#include "GUI.h"
 #include "TSensor.h"
 #include "Thread.h"
 
 // main() runs in its own thread in the OS
+SPI spi(D11, D12, D13);
+Screen screen(D14,D15,D5,D6,spi);
+GUI gui(screen);
+
 int main()
 {
-    cout<<"\nBegin"<<endl;
+    //cout<<"\nBegin"<<endl;
+    gui.init();
     
 }
